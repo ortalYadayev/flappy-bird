fbfa10bb5e130ff3640ba28411fb84e52cedcf8a
ortalll20111 <ortalll20111@gmail.com>
2020-03-28 19:34:08 +0300

    add README.md

--- src/app.ts
+++ src/app.ts
@@ -17,26 +17,131 @@ pipeTopEl.src = "images/pipe-top.png";
 const pipeBottomEl = new Image();
 pipeBottomEl.src = "images/pipe-bottom.png";
 
+const gameOverEl = new Image();
+gameOverEl.src = "images/game-over.png";
+
 const GAP = 85;
 let PIPES_DISTANCE: number;
 
+let birdX = 10;
+let birdY = 150;
+const gravity = 1.5;
+const JUMP = 30;
+
+let gameIsOver = false;
+
+type Pipe = {
+    x: number,
+    y: number,
+};
+
+let pipes: Pipe[] = [];
+
+function canvasClicked(): void {
+    if (gameIsOver) {
+        init();
+    } else {
+        birdJump();
+    }
+}
+
+function birdJump(): void {
+    if (birdY < JUMP) {
+        birdY = 0;
+        return;
+    }
+
+    birdY -= JUMP;
+}
+
 function init() {
     PIPES_DISTANCE = pipeTopEl.height + GAP;
+
+    pipes = [];
+    gameIsOver = false;
+    birdX = 10;
+    birdY = 150;
+
+    pushNewPipe();
+
+    canvasEl.addEventListener("click", canvasClicked);
 
     draw();
 }
 
 function draw() {
     context.drawImage(backgroundEl,0,0);
-
-    context.drawImage(pipeTopEl, 144, -50);
-    context.drawImage(pipeBottomEl, 144, -50 + PIPES_DISTANCE);
-
-    // context.drawImage(pipeBottomEl, pX, pY + gap);
     context.drawImage(foregroundEl, 0, canvasEl.height - foregroundEl.height);
-    // context.drawImage(birdEl, bX, bY);
 
-    // requestAnimationFrame(draw);
+    pipes.forEach(pipe => {
+        drawPipe(pipe);
+
+        pipe.x--;
+
+        if (isPipeCrossed100Px(pipe)) {
+            pushNewPipe();
+        }
+    });
+
+    context.drawImage(birdEl, birdX, birdY);
+    birdY += gravity;
+
+    if (birdTouchesPipes()) {
+        gameOver();
+    }
+
+    if (birdTouchesForeground()) {
+        gameOver();
+    }
+
+    if (!gameIsOver) {
+        requestAnimationFrame(draw);
+    }
+}
+
+function drawPipe(pipe: Pipe): void {
+    context.drawImage(pipeTopEl, pipe.x, pipe.y);
+    context.drawImage(pipeBottomEl, pipe.x, pipe.y + PIPES_DISTANCE);
+}
+
+function  isPipeCrossed100Px(pipe: Pipe) : boolean{
+    return pipe.x === canvasEl.width - 188;
+}
+
+function  pushNewPipe(): void {
+    pipes.push({
+        x: canvasEl.width,
+        y: getRandomHeight()
+    });
+}
+
+function getRandomHeight(): number {
+    const min = 0;
+    const max = pipeTopEl.height - 150;
+    return -(Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+function birdTouchesPipes(): boolean {
+    return pipes.some(birdTouchesPipe);
+}
+
+function birdTouchesPipe(pipe: Pipe): boolean {
+    return birdX + birdEl.width >= pipe.x
+        && birdX <= pipe.x + pipeTopEl.width
+        && (birdY <= pipe.y + pipeTopEl.height || birdY + birdEl.height >= pipe.y + PIPES_DISTANCE);
+}
+
+function birdTouchesForeground(): boolean{
+    return birdY + birdEl.height >= canvasEl.height - foregroundEl.height;
+}
+
+function gameOver(): void{
+    gameIsOver = true;
+
+    const width = canvasEl.width;
+    const height = canvasEl.width / 3.9;
+
+    context.drawImage(gameOverEl,0,canvasEl.height/4 - height/2, width, height);
 }
 
 window.onload = init;
